d3ffce33cfbe27d0d06380e2006efd70cbb39743
abdo <eldmokyabdo@gmail.com>
2021-01-18 11:51:55 +0200

    add some nice animations

--- js/main-trans.js
+++ js/main-trans.js
@@ -1,22 +1,60 @@
 "use strict";
 
-var navToggler = document.querySelectorAll('.nav-toggler');
+var navToggler = document.querySelector('.nav-toggler');
+var navTogglerSpan = document.querySelectorAll('.nav-toggler span');
+var brand = document.querySelector('.nav-brand');
 var links = document.querySelectorAll('.nav-list li');
+var navList = document.querySelector('.nav-list');
 var section = 'home';
 
-function toggleNav() {
+function toggleNavColor() {
+  setTimeout(function () {
+    brand.classList.toggle('brand-active');
+    navTogglerSpan.forEach(function (e) {
+      e.classList.toggle('nav-toggler-active');
+    });
+    brand.classList.remove('hide-element');
+    navTogglerSpan.forEach(function (e) {
+      e.classList.remove('hide-element');
+    });
+  }, 400);
+  brand.classList.add('hide-element');
+  navTogglerSpan.forEach(function (e) {
+    e.classList.add('hide-element');
+  });
+}
+
+function showNav() {
   //document.querySelector('.home .about').classList.toggle('hide-element')
-  document.querySelector(".".concat(section, " .background")).classList.toggle('background-open');
-  document.querySelector(".".concat(section, " .nav-list")).classList.toggle('list-open');
+  document.querySelector(".background").classList.add('background-open');
+  setTimeout(function () {
+    navList.classList.add('list-open');
+    toggleNavColor();
+  }, 300);
+  /* setTimeout(() => {
+      brand.classList.toggle('brand-active')
+      navTogglerSpan.forEach(e=>{
+          e.classList.toggle('nav-toggler-active')
+      })
+  }, 300); */
 }
 
 function removeNav() {
-  document.querySelector(".".concat(section, " .background")).classList.remove('background-open');
-  document.querySelector(".".concat(section, " .nav-list")).classList.remove('list-open');
+  navList.classList.remove('list-open');
+  setTimeout(function () {
+    document.querySelector(".background").classList.remove('background-open');
+    toggleNavColor();
+  }, 300);
 }
 
-navToggler.forEach(function (el) {
-  el.onclick = toggleNav;
+navToggler.addEventListener('click', function () {
+  if (navList.classList.contains('list-open')) {
+    removeNav();
+  } else {
+    showNav();
+  }
+
+  console.log(navList.classList);
 });
 
 function showSection(ele) {
@@ -24,6 +62,8 @@ function showSection(ele) {
     return document.querySelector("#".concat(el.dataset.section)).style = "display:none;";
   });
   document.querySelector("#".concat(ele.dataset.section)).style = "display:block";
+  document.body.classList.remove("".concat(section, "-colors"));
+  document.body.classList.add("".concat(ele.dataset.section, "-colors"));
   section = ele.dataset.section;
   removeNav();
 }
@@ -33,3 +73,13 @@ links.forEach(function (el) {
     showSection(el);
   };
 });
+
+function init() {
+  links.forEach(function (el) {
+    return document.querySelector("#".concat(el.dataset.section)).style = "display:none;";
+  });
+  document.querySelector("#".concat(section)).style = "display:block";
+  document.body.classList.add("".concat(section, "-colors"));
+}
+
+init();
--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
-const navToggler= document.querySelectorAll('.nav-toggler')
+const navToggler= document.querySelector('.nav-toggler')
+const navTogglerSpan= document.querySelectorAll('.nav-toggler span')
+const brand= document.querySelector('.nav-brand')
 const links= document.querySelectorAll('.nav-list li')
+const navList = document.querySelector('.nav-list')
 let section= 'home'
-function toggleNav() {
+
+function toggleNavColor() {
+    setTimeout(() => {
+        brand.classList.toggle('brand-active')
+        navTogglerSpan.forEach((e)=>{
+            e.classList.toggle('nav-toggler-active')
+        })
+        brand.classList.remove('hide-element')
+        navTogglerSpan.forEach((e)=>{
+            e.classList.remove('hide-element')
+         })
+    }, 400);
+    brand.classList.add('hide-element')
+    navTogglerSpan.forEach((e)=>{
+        e.classList.add('hide-element')
+    })
+}
+
+function showNav() {
     //document.querySelector('.home .about').classList.toggle('hide-element')
-    document.querySelector(`.${section} .background`).classList.toggle('background-open')
-    document.querySelector(`.${section} .nav-list`).classList.toggle('list-open')
+    document.querySelector(`.background`).classList.add('background-open')
+    setTimeout(() => {
+        navList.classList.add('list-open')
+        toggleNavColor()
+    }, 300);
+    /* setTimeout(() => {
+        brand.classList.toggle('brand-active')
+        navTogglerSpan.forEach(e=>{
+            e.classList.toggle('nav-toggler-active')
+        })
+    }, 300); */
 }
 function removeNav() {
-    document.querySelector(`.${section} .background`).classList.remove('background-open')
-    document.querySelector(`.${section} .nav-list`).classList.remove('list-open')
+    navList.classList.remove('list-open')
+    setTimeout(() => {
+        document.querySelector(`.background`).classList.remove('background-open')
+        toggleNavColor()
+    }, 300);
+
 }
 
-navToggler.forEach(el=>{
-    el.onclick= toggleNav
+navToggler.addEventListener('click', ()=>{
+    if(navList.classList.contains('list-open')){
+        removeNav()
+    }else{
+        showNav()
+    }
+    console.log(navList.classList)
+
 })
 
+ 
 function showSection(ele) {
     links.forEach((el)=>document.querySelector(`#${el.dataset.section}`).style= "display:none;")
     document.querySelector(`#${ele.dataset.section}`).style= "display:block"
+    document.body.classList.remove(`${section}-colors`)
+    document.body.classList.add(`${ele.dataset.section}-colors`)
+
     section= ele.dataset.section
     removeNav()
 }
@@ -26,3 +70,9 @@ links.forEach(el=>{
        showSection(el)   
     }
 })
+function init(){
+    links.forEach((el)=>document.querySelector(`#${el.dataset.section}`).style= "display:none;")
+    document.querySelector(`#${section}`).style= "display:block"
+    document.body.classList.add(`${section}-colors`)
+}
+init()
